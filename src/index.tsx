4df03b288ee29dea476fdc112a064def75c47890
Данила Корж <danila.schults@yandex.ru>
2023-11-07 20:05:06 +0300

    toggle theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { render } from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./theme/ThemeProvider";
-render(
+render (
     <BrowserRouter>
         <ThemeProvider>
             <App />
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 export const AboutPage: React.FC = () => {
   return (
     <div>AboutPage</div>
